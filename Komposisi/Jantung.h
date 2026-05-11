f02aaca02816bffaeecbcc4fafb519116c2e8b6e
risalahlele87-coder <risalahleles87@gmail.com>
2026-05-11 15:39:25 +0700

    menambahkan header guard dan namespace pada jantung.h

--- Komposisi/Jantung.h
+++ Komposisi/Jantung.h
@@ -3,13 +3,3 @@
 
 using namespace std;
 
-class Jantung {
-    public:
-    Jantung() {
-        cout << "Jantung dihidupkan\n" << endl;
-    }
-    ~Jantung() {
-        cout << "Jantung dimatikan\n" << endl;
-    }
-};
-#endif
