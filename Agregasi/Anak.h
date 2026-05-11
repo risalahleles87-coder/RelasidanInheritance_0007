73451e51678d1661b2f5ae6a150c2c618dc2c7b7
risalahlele87-coder <risalahleles87@gmail.com>
2026-05-11 15:30:44 +0700

    Menambahkan destructor class anak

--- Agregasi/Anak.h
+++ Agregasi/Anak.h
@@ -7,4 +7,6 @@ class anak {
     anak(string pNama) :nama(pNama) {
         cout << "Anak \"" << nama << "\" ada\n";
     }
-   
+    ~anak() {
+        cout << "Anak \"" << nama << "\" tidak ada\n";
+    }
