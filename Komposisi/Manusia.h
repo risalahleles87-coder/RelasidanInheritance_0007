2eb075ff260dbbcea691bb7eb53b58e2803a9c7a
risalahlele87-coder <risalahleles87@gmail.com>
2026-05-11 15:43:02 +0700

    menambahkan class manusia dengan object jantung dan menutup helder

--- Komposisi/Manusia.h
+++ Komposisi/Manusia.h
@@ -2,3 +2,16 @@
 #define MANUSIA_H
 using namespace std;
 
+class manusia {
+public:
+    string name;
+    jantung varjantung;
+
+    manusia(string pName) :nama(pName) {
+        cout << name << "hidup\n"
+    }
+    ~manusia() {
+        cout << name << "mati\n"
+    }
+};
+#endif
