b42804142087cbf5738160bade8385a978246235
risalahlele87-coder <risalahleles87@gmail.com>
2026-05-11 15:34:44 +0700

    Menambahkan constructor dan destructor pada class ibu

--- Agregasi/Ibu.h
+++ Agregasi/Ibu.h
@@ -7,4 +7,10 @@ class ibu {
     string nama;
     vector<anak*> daftar_anak;
 
+    ibu(string pNama) :nama(pNama) {
+        cout << "Ibu \"" << nama << "\" ada\n";
+    }
+    ~ibu() {
+        cout << "Ibu \"" << nama << "\" tidak ada\n";
+    }
     
