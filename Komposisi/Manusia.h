f90a2e18d2c95844af4bbfc8265bd11e202140cf
risalahlele87-coder <risalahleles87@gmail.com>
2026-05-11 15:42:25 +0700

    menambahkan header guard pada manusia.h

--- Komposisi/Manusia.h
+++ Komposisi/Manusia.h
@@ -2,16 +2,3 @@
 #define MANUSIA_H
 using namespace std;
 
-class manusia {
-public:
-    string name;
-    jantung varjantung;
-
-    manusia(string pName) :nama(pName) {
-        cout << name << "hidup\n"
-    }
-    ~manusia() {
-        cout << name << "mati\n"
-    }
-};
-#endif
