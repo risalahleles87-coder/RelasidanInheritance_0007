72fd34adce474d70fab40b1e7c47c595d6349147
risalahlele87-coder <risalahleles87@gmail.com>
2026-05-11 15:40:00 +0700

    menambahkan class Jantung beserta constructor dan destructor

--- Komposisi/Jantung.h
+++ Komposisi/Jantung.h
@@ -3,3 +3,11 @@
 
 using namespace std;
 
+class Jantung {
+    public:
+    Jantung() {
+        cout << "Jantung dihidupkan\n" << endl;
+    }
+    ~Jantung() {
+        cout << "Jantung dimatikan\n" << endl;
+    }
