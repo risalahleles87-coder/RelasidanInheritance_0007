26c07e798f0e9c2b5c127d4d32b14893ee9f099c
risalahlele87-coder <risalahleles87@gmail.com>
2026-05-11 15:37:01 +0700

    menambahkan implementasi function cetakAnak dan menutup program

--- Agregasi/Ibu.h
+++ Agregasi/Ibu.h
@@ -19,3 +19,14 @@ class ibu {
 void ibu::tambahAnak(anak* pAnak) {
     daftar_anak.push_back(pAnak);
 }
+void ibu::cetakAnak() {
+    cout << "Daftar Anak dari ibu \"" << this->nama << "\":\n";
+    /*for (auto& a : daftar_anak) {
+        cout << a->nama << "\n";
+    }*/
+   for (int i = 0; i < daftar_anak.size(); i++) {
+        cout << daftar_anak[i]->nama << endl;
+   }
+   cout << endl;
+}
+#endif
